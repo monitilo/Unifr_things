4fcc334f6f3dba8a776e7d0fcf61aaa09474f727
CHIARELLI German <german.chiarelli@unifr.ch>
2021-06-24 13:51:17 +0200

    chage name of file to use a function from another file

---
+++ fiting_functions.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 13:32:39 2021
+
+@author: chiarelg
+"""
+
+def plot_histo_fit(vector, bines, name):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    import pylab as plb
+    from scipy.optimize import curve_fit
+    from scipy import asarray as ar,exp
+    N = len(vector)/bines
+    bins = np.linspace(0,int(np.max(vector)), N)
+    
+    plt.hist(vector, bins=bins, alpha = 0.5, label=name)# , color="#900090",alpha=0.6,label='data')  # len(nozeros)//N
+    y,x = np.histogram(vector, bins=bins)  #len(nozeros)//N
+    x=(x[1:]+x[:-1])/2 # for len(x)==len(y)
+    
+    n = len(x)                          #the number of data
+    mean = sum(x*y)/sum(y)                   #note this correction
+    sigma = sum(y*(x-mean)**2)/sum(y)        #note this correction
+    
+    def gaus(x,a,x0,sigma):
+        return a*exp(-(x-x0)**2/(2*sigma**2))
+    
+    popt,pcov = curve_fit(gaus,x,y,p0=[1,mean,sigma/100])
+    perr = np.sqrt(np.diag(pcov))
+    
+    
+    #plt.plot(x,y,'b+:',label='data')
+    X = np.linspace(x[0], x[-1], 500)
+    plt.plot()
+    plt.plot(X,gaus(X,*popt),'g',lw=2, label='1G fit')
+    plt.vlines(popt[1], color="k", ymin=0,ymax=0.5*popt[0])
+    plt.vlines((popt[1]-popt[2], popt[1]+popt[2]),color='orange', ymin=0, ymax=10)
+    plt.legend()
+    plt.title('hist')
+    plt.xlabel('Counts kHz')
+    plt.ylabel("total points ={} in {} bins".format(len(vector), N))
+    plt.xlim([0,3000])
+    #    plt.text(30,50, "mean ={:.2f}±{:.2f}".format(popt[1], popt[2]))
+    print(popt[1], popt[2], "1")
+    #plt.xlim(np.min(x), popt[1]+abs(popt[2]*3))
+    #plt.xlim(0, int(np.max(nozeros)))
+    #plt.show()
+        
+    
+    def gauss(x,mu,sigma,A):
+        return A*exp(-(x-mu)**2/2/sigma**2)
+    
+    def bimodal(x,mu1,sigma1,A1,mu2,sigma2,A2):
+        return gauss(x,mu1,sigma1,A1)+gauss(x,mu2,sigma2,A2)
+    
+    
+    expected = (popt[1],abs(popt[2]),popt[0],
+                0.5*popt[1], 0.5*abs(popt[2]), 0.5*popt[0])
+    
+    
+    try:
+        params,cov = curve_fit(bimodal,x,y,expected)
+        sigma = np.sqrt(np.diag(cov))
+    
+        if params[0] < 0 or params[3] < 0:
+            print("bad adjusts 2G")
+        else:
+            #X = np.linspace(x[0]-50, x[-1]+50, 5000)
+            plt.plot(X,bimodal(X,*params),color='orange',lw=3,label='2G model')
+            plt.legend()
+            plt.vlines((params[0], params[3]), color=('r','b'), ymin=0,ymax=0.5*popt[0])
+            
+#        print(params,'\n',sigma)
+        #print("\n mal Gauss", (viejopopt[1],"±", viejopopt[2]),"*",viejopopt[0])
+    except:
+        params = ["no"]*6
+    
+#    print("\n 1Gaus=",(popt[1],"±", popt[2]), "*", popt[0])
+#    print("\n 2Gaus=",(params[0], "±", params[1]), "*", params[2],
+#              "\n",(params[3],"±",params[4]), "*", params[5])
+
+    return (int(params[0]), int(params[3]))
--- photons_circles.py
+++ photons_circles.py
@@ -164,7 +164,7 @@ for s in range(len(samples)):
                 finaldata[samples[s]][photons_circles[c]].append(finaldata[samples[s]]["photons"][i])
                 finaldata[samples[s]][bg_circles[c]].append(finaldata[samples[s]]["bg"][i])
     
-        print("\n c=",c,";time=", time.time()-tic, ":")
+        print("\n s=",s," c=",c,";time=", time.time()-tic, ":")
     
     
         hist2d = plt.hist2d(finaldata[samples[s]][x_circles[c]], finaldata[samples[s]][y_circles[c]], bins=bines, range=([0,154], [0,154]), cmin=0, cmax=4000)
@@ -191,191 +191,131 @@ plt.legend()
 plt.show()
 
 #%%
-from
+from fiting_functions import plot_histo_fit
+
 data = finaldata[samples[0]][photons_circles[0]]
 
 bines = 100
-
-(gauss1, gauss2) = plot_histo_fit(data, bines)
-
-#%%
-import numpy as np
-import matplotlib.pyplot as plt
-
-import pylab as plb
-from scipy.optimize import curve_fit
-from scipy import asarray as ar,exp
-
-#N=[50,200,200]
-largos = []
+gauss1 = np.zeros((N_circles))
+gauss2 = np.zeros((N_circles))
+gauss3 = np.zeros((N_circles))
+gauss4 = np.zeros((N_circles))
 for c in range(N_circles):
-    largos.append(len(finaldata[samples[s]][photons_circles[c]]))
-largos = np.array(largos)
-
-bines = 100
-auxe = largos / bines
-largos/auxe
-
-nbins = auxe #[10]*4 # [10, 10, 10, 10, 10, 10, 10]
-
-c=0
-fit_max = np.zeros((N_circles))
-fit_min = np.zeros((N_circles))
+    (gauss1[c], gauss2[c]) = plot_histo_fit(finaldata[samples[0]][photons_circles[c]], bines, photons_circles[c])
+plt.show()
 
 for c in range(N_circles):
-    N = nbins[c]
-    bins = np.linspace(0,int(np.max(phothons_circle[c])), N)
-    
-    plt.hist(phothons_circle[c], bins=bins, alpha = 0.5, label="Photons_circle {}".format(c))# , color="#900090",alpha=0.6,label='data')  # len(nozeros)//N
-    y,x = np.histogram(phothons_circle[c], bins=bins)  #len(nozeros)//N
-    x=(x[1:]+x[:-1])/2 # for len(x)==len(y)
-    
-    n = len(x)                          #the number of data
-    mean = sum(x*y)/sum(y)                   #note this correction
-    sigma = sum(y*(x-mean)**2)/sum(y)        #note this correction
-    
-    def gaus(x,a,x0,sigma):
-        return a*exp(-(x-x0)**2/(2*sigma**2))
-    
-    popt,pcov = curve_fit(gaus,x,y,p0=[1,mean,sigma/100])
-    perr = np.sqrt(np.diag(pcov))
-    
-    
-    #plt.plot(x,y,'b+:',label='data')
-    X = np.linspace(x[0], x[-1], 500)
-    plt.plot()
-    plt.plot(X,gaus(X,*popt),'g',lw=2, label='1G fit')
-    plt.vlines(popt[1], color="k", ymin=0,ymax=0.5*popt[0])
-    plt.vlines((popt[1]-popt[2], popt[1]+popt[2]),color='orange', ymin=0, ymax=10)
-    plt.legend()
-    plt.title('hist')
-    plt.xlabel('Counts kHz')
-    plt.ylabel("total points ={} in {} bins".format(len(phothons_circle[c]), N))
-    plt.xlim([0,3000])
-    #    plt.text(30,50, "mean ={:.2f}±{:.2f}".format(popt[1], popt[2]))
-    print(popt[1], popt[2], "1")
-    #plt.xlim(np.min(x), popt[1]+abs(popt[2]*3))
-    #plt.xlim(0, int(np.max(nozeros)))
-    #plt.show()
-        
-    
-    def gauss(x,mu,sigma,A):
-        return A*exp(-(x-mu)**2/2/sigma**2)
-    
-    def bimodal(x,mu1,sigma1,A1,mu2,sigma2,A2):
-        return gauss(x,mu1,sigma1,A1)+gauss(x,mu2,sigma2,A2)
-    
-    
-    expected = (popt[1],abs(popt[2]),popt[0],
-                0.5*popt[1], 0.5*abs(popt[2]), 0.5*popt[0])
-    
-    
     try:
-        params,cov = curve_fit(bimodal,x,y,expected)
-        sigma = np.sqrt(np.diag(cov))
-    
-        if params[0] < 0 or params[3] < 0:
-            print("bad adjusts 2G")
-        else:
-            #X = np.linspace(x[0]-50, x[-1]+50, 5000)
-            plt.plot(X,bimodal(X,*params),color='orange',lw=3,label='2G model')
-            plt.legend()
-            plt.vlines((params[0], params[3]), color=('r','b'), ymin=0,ymax=0.5*popt[0])
-#        print(params,'\n',sigma)
-        #print("\n mal Gauss", (viejopopt[1],"±", viejopopt[2]),"*",viejopopt[0])
+        (gauss3[c], gauss4[c]) = plot_histo_fit(finaldata[samples[1]][photons_circles[c]], bines, photons_circles[c])
     except:
-        params = ["no"]*6
-    
-#    print("\n 1Gaus=",(popt[1],"±", popt[2]), "*", popt[0])
-#    print("\n 2Gaus=",(params[0], "±", params[1]), "*", params[2],
-#              "\n",(params[3],"±",params[4]), "*", params[5])
-    fit_max[c] = int(params[0])
-    fit_min[c] = int(params[3])
+        pass
+plt.show()
 
-print(fit_max)
-print("\n",fit_min)
+print("\n", gauss1, gauss2,"\n", gauss3, gauss4)
+
+#plt.plot(gauss1,".-")
+##plt.plot(gauss2,".-")
+#plt.plot(gauss3,".-")
+##plt.plot(gauss4,".-")
+
+#%%
+# =============================================================================
+# import numpy as np
+# import matplotlib.pyplot as plt
+# 
+# import pylab as plb
+# from scipy.optimize import curve_fit
+# from scipy import asarray as ar,exp
+# 
+# #N=[50,200,200]
+# largos = []
+# for c in range(N_circles):
+#     largos.append(len(finaldata[samples[s]][photons_circles[c]]))
+# largos = np.array(largos)
+# 
+# bines = 100
+# auxe = largos / bines
+# largos/auxe
+# 
+# nbins = auxe #[10]*4 # [10, 10, 10, 10, 10, 10, 10]
+# 
+# c=0
+# fit_max = np.zeros((N_circles))
+# fit_min = np.zeros((N_circles))
+# 
+# for c in range(N_circles):
+#     N = nbins[c]
+#     bins = np.linspace(0,int(np.max(phothons_circle[c])), N)
+#     
+#     plt.hist(phothons_circle[c], bins=bins, alpha = 0.5, label="Photons_circle {}".format(c))# , color="#900090",alpha=0.6,label='data')  # len(nozeros)//N
+#     y,x = np.histogram(phothons_circle[c], bins=bins)  #len(nozeros)//N
+#     x=(x[1:]+x[:-1])/2 # for len(x)==len(y)
+#     
+#     n = len(x)                          #the number of data
+#     mean = sum(x*y)/sum(y)                   #note this correction
+#     sigma = sum(y*(x-mean)**2)/sum(y)        #note this correction
+#     
+#     def gaus(x,a,x0,sigma):
+#         return a*exp(-(x-x0)**2/(2*sigma**2))
+#     
+#     popt,pcov = curve_fit(gaus,x,y,p0=[1,mean,sigma/100])
+#     perr = np.sqrt(np.diag(pcov))
+#     
+#     
+#     #plt.plot(x,y,'b+:',label='data')
+#     X = np.linspace(x[0], x[-1], 500)
+#     plt.plot()
+#     plt.plot(X,gaus(X,*popt),'g',lw=2, label='1G fit')
+#     plt.vlines(popt[1], color="k", ymin=0,ymax=0.5*popt[0])
+#     plt.vlines((popt[1]-popt[2], popt[1]+popt[2]),color='orange', ymin=0, ymax=10)
+#     plt.legend()
+#     plt.title('hist')
+#     plt.xlabel('Counts kHz')
+#     plt.ylabel("total points ={} in {} bins".format(len(phothons_circle[c]), N))
+#     plt.xlim([0,3000])
+#     #    plt.text(30,50, "mean ={:.2f}±{:.2f}".format(popt[1], popt[2]))
+#     print(popt[1], popt[2], "1")
+#     #plt.xlim(np.min(x), popt[1]+abs(popt[2]*3))
+#     #plt.xlim(0, int(np.max(nozeros)))
+#     #plt.show()
+#         
+#     
+#     def gauss(x,mu,sigma,A):
+#         return A*exp(-(x-mu)**2/2/sigma**2)
+#     
+#     def bimodal(x,mu1,sigma1,A1,mu2,sigma2,A2):
+#         return gauss(x,mu1,sigma1,A1)+gauss(x,mu2,sigma2,A2)
+#     
+#     
+#     expected = (popt[1],abs(popt[2]),popt[0],
+#                 0.5*popt[1], 0.5*abs(popt[2]), 0.5*popt[0])
+#     
+#     
+#     try:
+#         params,cov = curve_fit(bimodal,x,y,expected)
+#         sigma = np.sqrt(np.diag(cov))
+#     
+#         if params[0] < 0 or params[3] < 0:
+#             print("bad adjusts 2G")
+#         else:
+#             #X = np.linspace(x[0]-50, x[-1]+50, 5000)
+#             plt.plot(X,bimodal(X,*params),color='orange',lw=3,label='2G model')
+#             plt.legend()
+#             plt.vlines((params[0], params[3]), color=('r','b'), ymin=0,ymax=0.5*popt[0])
+# #        print(params,'\n',sigma)
+#         #print("\n mal Gauss", (viejopopt[1],"±", viejopopt[2]),"*",viejopopt[0])
+#     except:
+#         params = ["no"]*6
+#     
+# #    print("\n 1Gaus=",(popt[1],"±", popt[2]), "*", popt[0])
+# #    print("\n 2Gaus=",(params[0], "±", params[1]), "*", params[2],
+# #              "\n",(params[3],"±",params[4]), "*", params[5])
+#     fit_max[c] = int(params[0])
+#     fit_min[c] = int(params[3])
+# 
+# print(fit_max)
+# print("\n",fit_min)
+# =============================================================================
 
 #%%
 
-bines = 100
-#auxe = largos / bines
-#largos/auxe
-
-#nbins = auxe #[10]*4 # [10, 10, 10, 10, 10, 10, 10]
-
-c=0
-
-def plot_histo_fit(vector, bines):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    
-    import pylab as plb
-    from scipy.optimize import curve_fit
-    from scipy import asarray as ar,exp
-    N = len(vector)/bines
-    bins = np.linspace(0,int(np.max(vector)), N)
-    
-    plt.hist(vector, bins=bins, alpha = 0.5, label="Photons_circle {}".format(c))# , color="#900090",alpha=0.6,label='data')  # len(nozeros)//N
-    y,x = np.histogram(vector, bins=bins)  #len(nozeros)//N
-    x=(x[1:]+x[:-1])/2 # for len(x)==len(y)
-    
-    n = len(x)                          #the number of data
-    mean = sum(x*y)/sum(y)                   #note this correction
-    sigma = sum(y*(x-mean)**2)/sum(y)        #note this correction
-    
-    def gaus(x,a,x0,sigma):
-        return a*exp(-(x-x0)**2/(2*sigma**2))
-    
-    popt,pcov = curve_fit(gaus,x,y,p0=[1,mean,sigma/100])
-    perr = np.sqrt(np.diag(pcov))
-    
-    
-    #plt.plot(x,y,'b+:',label='data')
-    X = np.linspace(x[0], x[-1], 500)
-    plt.plot()
-    plt.plot(X,gaus(X,*popt),'g',lw=2, label='1G fit')
-    plt.vlines(popt[1], color="k", ymin=0,ymax=0.5*popt[0])
-    plt.vlines((popt[1]-popt[2], popt[1]+popt[2]),color='orange', ymin=0, ymax=10)
-    plt.legend()
-    plt.title('hist')
-    plt.xlabel('Counts kHz')
-    plt.ylabel("total points ={} in {} bins".format(len(vector), N))
-    plt.xlim([0,3000])
-    #    plt.text(30,50, "mean ={:.2f}±{:.2f}".format(popt[1], popt[2]))
-    print(popt[1], popt[2], "1")
-    #plt.xlim(np.min(x), popt[1]+abs(popt[2]*3))
-    #plt.xlim(0, int(np.max(nozeros)))
-    #plt.show()
-        
-    
-    def gauss(x,mu,sigma,A):
-        return A*exp(-(x-mu)**2/2/sigma**2)
-    
-    def bimodal(x,mu1,sigma1,A1,mu2,sigma2,A2):
-        return gauss(x,mu1,sigma1,A1)+gauss(x,mu2,sigma2,A2)
-    
-    
-    expected = (popt[1],abs(popt[2]),popt[0],
-                0.5*popt[1], 0.5*abs(popt[2]), 0.5*popt[0])
-    
-    
-    try:
-        params,cov = curve_fit(bimodal,x,y,expected)
-        sigma = np.sqrt(np.diag(cov))
-    
-        if params[0] < 0 or params[3] < 0:
-            print("bad adjusts 2G")
-        else:
-            #X = np.linspace(x[0]-50, x[-1]+50, 5000)
-            plt.plot(X,bimodal(X,*params),color='orange',lw=3,label='2G model')
-            plt.legend()
-            plt.vlines((params[0], params[3]), color=('r','b'), ymin=0,ymax=0.5*popt[0])
-#        print(params,'\n',sigma)
-        #print("\n mal Gauss", (viejopopt[1],"±", viejopopt[2]),"*",viejopopt[0])
-    except:
-        params = ["no"]*6
-    
-#    print("\n 1Gaus=",(popt[1],"±", popt[2]), "*", popt[0])
-#    print("\n 2Gaus=",(params[0], "±", params[1]), "*", params[2],
-#              "\n",(params[3],"±",params[4]), "*", params[5])
-    return (int(params[0]), int(params[3]))
